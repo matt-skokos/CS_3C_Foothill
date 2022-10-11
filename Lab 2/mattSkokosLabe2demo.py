b3be8143adadbe82de4065e789dde06a9fa6fbb2
matt <matthew.skokos@gmail.com>
2022-10-11 13:00:05 -0700

    lab 2 commit

--- Lab 2/mattSkokosLabe2demo.py
+++ Lab 2/mattSkokosLabe2demo.py
@@ -1,9 +1,24 @@
 from mattSkokosLab2 import Translator
 
+
+def timer(self, func):
+    """ Runs the program functions in a simple benchmarking
+    structure.  The return value will appear after the included
+    functions return their output.
+    """
+    start = time.time()
+    func()
+    print("calculate time between here")
+
+    stop = time.time()
+    print(f"time: {(stop - start):2.7f}")
+    return f'Run-time: {stop - start}'
+
+
 def main():
     translate1 = Translator()
-    translate1.load_file('latin.txt')
-    translate1.timer()
+    # translate1.load_file('latin.txt')
+    translate1.timer(translate1.load_file('latin.txt'))
 
 
 if __name__ == "__main__":
@@ -13,6 +28,5 @@ if __name__ == "__main__":
 strange question but is strip better than or the same as replace here?
 latin = latin.strip(',')
 latin = latin.replace(',', '')
-
 
 """
